4d210cbc5439c90bd251a24b7cb1a101553a008e
hmuronaka <capriccio27@gmail.com>
2015-09-03 19:25:33 +0900

    test for cartago

---
+++ HMStopWatch-Framework/HMStopWatch-Framework.h
@@ -0,0 +1,19 @@
+//
+//  HMStopWatch-Framework.h
+//  HMStopWatch-Framework
+//
+//  Created by Muronaka Hiroaki on 2015/09/03.
+//  Copyright (c) 2015年 hmu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HMStopWatch-Framework.
+FOUNDATION_EXPORT double HMStopWatch_FrameworkVersionNumber;
+
+//! Project version string for HMStopWatch-Framework.
+FOUNDATION_EXPORT const unsigned char HMStopWatch_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMStopWatch_Framework/PublicHeader.h>
+
+
---
+++ HMStopWatchFramework/HMStopWatchFramework.h
@@ -0,0 +1,19 @@
+//
+//  HMStopWatchFramework.h
+//  HMStopWatchFramework
+//
+//  Created by Muronaka Hiroaki on 2015/09/03.
+//  Copyright (c) 2015年 hmu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HMStopWatchFramework.
+FOUNDATION_EXPORT double HMStopWatchFrameworkVersionNumber;
+
+//! Project version string for HMStopWatchFramework.
+FOUNDATION_EXPORT const unsigned char HMStopWatchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMStopWatchFramework/PublicHeader.h>
+
+
